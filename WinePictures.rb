e1d3114a86487f9919e89f31a09ae16ea07ebb48
David Burke <dburke@amberfire.net>
2015-08-17 20:55:39 -0700

    Finished App

--- WinePictures.rb
+++ WinePictures.rb
@@ -1,43 +1,76 @@
 require 'google-search'
 require 'csv.rb'
-require 'FileNameUtils.rb' 
+require 'open-uri'
+require 'open_uri_redirections'
+require 'net/https'  
 
 module WinePictures
   
+  class FileNameUtil
+    LABEL_PATH_NAME = "D:/Users/David/Pictures/WineLabels/"
+         
+    def isLabelPresent (labelName)
+      fqn = LABEL_PATH_NAME + labelName
+      return !Dir.glob(fqn).empty?
+    end   
+    
+    def fullyQualifiedName (fileName)
+      return LABEL_PATH_NAME + fileName
+    end
+  end
+    
   class WineFile
     
     PATH_NAME = "D:/Users/David/Workspace/WinePictures/"
     FILE_NAME = "export-bottles-stored.csv"
+    WILD_CARD = "*"
    
     @lastBottle = String.new()
-    @fnu = FileNameUtils::FileNameUtils.new()
-    
+    @@fnu = FileNameUtil.new()
+   
     def readFile
       fqn = PATH_NAME + FILE_NAME
       i=0
       bottleDesc = Array.new()
       
       CSV.foreach(fqn,  encoding: "bom|UTF-8", :row_sep => :auto, :headers => true) do |bottle|   
-          bottleDesc[i] = "#{bottle['Winery']} #{bottle['Name']} label"
-          i += 1
+          if bottle['Name'] != ""
+            desc = "#{bottle['Winery']} #{bottle['Name']} label"
+          else
+            desc = "#{bottle['Winery']} #{bottle['Grapes']} label"
+          end
+          bottleDesc[i] = BottleName.new(desc)
+          i +=  1
       end
-     return bottleDesc
+      return bottleDesc
     end
     
     def isNewBottle(bottle)
-      if bottle == @lastBottle
+
+      if bottle.searchName == @lastBottle
            return false
       end
-      @lastBottle = bottle   
-      return true
+      @lastBottle = bottle.searchName   
+      
+      if @@fnu.isLabelPresent(bottle.fileName(WILD_CARD))
+        return false
+      else
+        bottle.setUri()
+        return true
+      end
+    
     end
+    
   end
+  
+  
   
   class BottleName
     
     def initialize(name)
-      @searchName = name
-      @fileRoot = name.to_s.gsub(" ","_")
+      @searchName = name.gsub("'","")
+      @fileRoot = @searchName.to_s.gsub(" ","_")
+      @uri =""
     end
     
     def searchName
@@ -49,15 +82,39 @@ module WinePictures
     end
       
     def fileName (ext)
-      return fileRoot + "." + ext
+      return fileRoot + ext
+    end
+    
+    def setUri
+      @uri = Google::Search::Image.new(:query => @searchName).first.uri 
+    end
+    
+    def uri
+      return @uri
     end
     
   end
   
+ 
+  WRITE_BINARY = "wb"
+  fnu = FileNameUtil.new 
   wf = WineFile.new
-  searchString = wf.readFile()
+  bottles = wf.readFile()
   
-  searchString.sort!
-  puts searchString[30]
+  for index in 0 ... bottles.size
+    bottle = bottles[index]
+    if wf.isNewBottle(bottle)
+      ext =  File.extname(bottle.uri)
+      qm = ext.index("?")
+      if !qm.nil?
+        ext = ext[0 ... qm]
+      end 
+      fileName = bottle.fileName(ext)
+      puts fnu.fullyQualifiedName(fileName)
+      File.open(fnu.fullyQualifiedName(fileName), WRITE_BINARY) do |fo|
+        fo.write open(bottle.uri, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,  :allow_redirections => :all}).read 
+      end
+    end
+  end
   
-end
+end 
--- testWinePictures.rb
+++ testWinePictures.rb
@@ -1,25 +1,27 @@
 require 'test/unit'
 require 'WinePictures'
-require 'FileNameUtils.rb' 
+
 
 module TestWinePictures
   
   class TestIsNewBottle < Test::Unit::TestCase
     def testIsNewBottle 
       wf = WinePictures::WineFile.new()
-      result = wf.isNewBottle("Bottle")
+      result = wf.isNewBottle(WinePictures::BottleName.new("Bottle"))
       assert_equal result, true
-      result = wf.isNewBottle("Bottle")
+      result = wf.isNewBottle(WinePictures::BottleName.new("Bottle"))
+      assert_equal result, false 
+      result = wf.isNewBottle(WinePictures::BottleName.new("Bottle2"))
+      assert_equal result, true
+      result = wf.isNewBottle(WinePictures::BottleName.new("Unit_Test"))
       assert_equal result, false
-      result = wf.isNewBottle("Bottle2")
-      assert_equal result, true
     end
   end
   
   class TestIsLabelPresent < Test::Unit::TestCase
     
     def testisLabelPresent
-      fnu = FileNameUtils::FileNameUtils.new()
+      fnu =  WinePictures::FileNameUtil.new()
       result = fnu.isLabelPresent("Unit_Test.*")
       assert_equal result, true
       result = fnu.isLabelPresent("Unit_Test_false.*")
@@ -34,10 +36,26 @@ module TestWinePictures
       assert_equal result, "Unit Test"
       result = tbn.fileRoot
       assert_equal result, "Unit_Test"
-      result = tbn.fileName("jpg")
+      result = tbn.fileName(".jpg")
       assert_equal result, "Unit_Test.jpg"  
     end
   end
- 
+  
+  class TestBottleNameApostrophe < Test::Unit::TestCase
+      def testBottleName
+        tbn = WinePictures::BottleName.new("Lover's")
+        result = tbn.searchName
+        assert_equal result, "Lovers"
+      end
+  end
+  
+  class TestSetUri < Test::Unit::TestCase
+    def testSetUri
+      tbn = WinePictures::BottleName.new("Castle Rock Syrah label")
+      tbn.setUri
+      result = tbn.uri
+      assert_equal result, "http://sr1.wine-searcher.net/images/labels/92/68/10249268.jpg"
+    end
+  end
   
 end
